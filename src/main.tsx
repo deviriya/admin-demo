4798fee01103eb6cccea34204a00b8ccfd3bac3b
deviriya <devipriyaclouds2000@gmail.com>
2023-12-14 19:09:16 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css';
-// import 'aos/dist/aos.css';
+import 'aos/dist/aos.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
+import '../src/input.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
